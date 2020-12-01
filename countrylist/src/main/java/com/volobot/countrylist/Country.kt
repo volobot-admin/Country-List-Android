16cb3cb8f6995d51165631bad89cb5e5d09d3981
Gurjap Singh <sjapgurjap@gmail.com>
2020-12-01 09:25:12 +0530

    updated to kotlin

--- countrylist/src/main/java/com/volobot/countrylist/Country.kt
+++ countrylist/src/main/java/com/volobot/countrylist/Country.kt
@@ -1,31 +1,7 @@
-package com.volobot.countrylist;
+package com.volobot.countrylist
 
-public class Country {
-    private String name;
-    private String dial_code;
-    private String code;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDial_code() {
-        return dial_code;
-    }
-
-    public void setDial_code(String dial_code) {
-        this.dial_code = dial_code;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
+class Country {
+    var name: String? = null
+    var dial_code: String? = null
+    var code: String? = null
 }
